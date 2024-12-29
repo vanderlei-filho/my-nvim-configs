33c566e977e3b1d3b558183729a7529fe75bdb1a
Vanderlei <vanderlei.filho@proton.me>
2024-12-29 00:07:39 -0300

    Update Git shortcuts and show hidden files in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,16 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- Show hidden files
+					show_hidden = true, -- Specifically show dotfiles
+					hide_dotfiles = false, -- Disable hiding dotfiles
+					hide_gitignored = false, -- Optionally show git-ignored files
+				},
+			},
+		})
 		vim.keymap.set("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", { desc = "Open cwd file tree" })
 	end,
 }
